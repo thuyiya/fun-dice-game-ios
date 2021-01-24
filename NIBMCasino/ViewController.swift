a7c015a520ac6c73575f30da951ae8b976336ee0
Thusitha Jayalath <thusithajayalath@Thusithas-iMac.local>
2021-01-24 09:34:38 +0530

    init project

---
+++ NIBMCasino/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NIBMCasino
+//
+//  Created by Thusitha Jayalath on 1/24/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
